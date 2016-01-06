63732d0b27d061285bc6829463d0e698128feb9c
daljit shergill <d4lj1t@hotmail.com>
2016-01-06 20:46:56 +0000

    changes css files to less

---
+++ Website/assets/scripts/main.js
@@ -0,0 +1,36 @@
+﻿function bindMobileNav() {
+    $(".js-mobile-menu-trigger").click(function () {
+        var trigger = $(this);
+        var target = $(".mobile-navigations-wrapper, .global-container");
+        if (trigger.hasClass("active")) {
+            trigger.removeClass("active");
+            target.animate({ marginLeft: "-=267" }, 500);
+        } else {
+            trigger.addClass("active");
+            target.animate({ marginLeft: "+=267" }, 500);
+        }
+    });
+
+
+    $(".main-nav-item .js-accordion-indicator").click(function () {
+        var trigger = $(this);
+        var wrapper = $(this).closest(".main-nav-item");
+        var target = wrapper.find(".main-nav-dropdown");
+        if (!wrapper.hasClass("active")) {
+            target.slideDown();
+            wrapper.addClass("active");
+        } else {
+            target.slideUp();
+            wrapper.removeClass("active");
+        }
+    });
+}
+
+$(document).ready(function () {
+    bindMobileNav();
+});
+
+
+
+
+
