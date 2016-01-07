acb9ddde5f9d5b1055a788fc89223075caca92d5
daljit shergill <d4lj1t@hotmail.com>
2016-01-07 14:22:33 +0000

    js for the thumbs up and down working as should

--- Website/assets/scripts/main.js
+++ Website/assets/scripts/main.js
@@ -1,33 +1,24 @@
-﻿function bindMobileNav() {
-    $(".js-mobile-menu-trigger").click(function () {
-        var trigger = $(this);
-        var target = $(".mobile-navigations-wrapper, .global-container");
-        if (trigger.hasClass("active")) {
-            trigger.removeClass("active");
-            target.animate({ marginLeft: "-=267" }, 500);
-        } else {
-            trigger.addClass("active");
-            target.animate({ marginLeft: "+=267" }, 500);
+﻿
+
+function bindClickEvents() {
+
+    $(".js-thumb-up").click(function() {
+        if ($(this).parent().find(".js-thumb-val").val() >= 0 && $(this).parent().find(".js-thumb-val").val() < 10 ) {
+            $(this).parent().find(".js-thumb-val").val(+$(this).parent().find(".js-thumb-val").val() + 1)
         }
     });
 
-
-    $(".main-nav-item .js-accordion-indicator").click(function () {
-        var trigger = $(this);
-        var wrapper = $(this).closest(".main-nav-item");
-        var target = wrapper.find(".main-nav-dropdown");
-        if (!wrapper.hasClass("active")) {
-            target.slideDown();
-            wrapper.addClass("active");
-        } else {
-            target.slideUp();
-            wrapper.removeClass("active");
+    $(".js-thumb-down").click(function() {
+        if ($(this).parent().find(".js-thumb-val").val() > 0 && $(this).parent().find(".js-thumb-val").val() <= 10 ) {
+            $(this).parent().find(".js-thumb-val").val(+$(this).parent().find(".js-thumb-val").val() - 1)
         }
     });
 }
 
 $(document).ready(function () {
-    bindMobileNav();
+    bindClickEvents();
+
+
 });
 
 
