a208f60d441512ad60cd2d4026ed6ba7994022c9
daljit shergill <d4lj1t@hotmail.com>
2016-01-07 19:42:40 +0000

    added form to thumb inputs, js put in mvc format (wip)

--- Website/assets/scripts/main.js
+++ Website/assets/scripts/main.js
@@ -1,24 +1,51 @@
-﻿
+﻿function bindClickEvents() {
 
-function bindClickEvents() {
-
-    $(".js-thumb-up").click(function() {
-        if ($(this).parent().find(".js-thumb-val").val() >= 0 && $(this).parent().find(".js-thumb-val").val() < 10 ) {
+    $(".js-thumb-up").click(function () {
+        if ($(this).parent().find(".js-thumb-val").val() >= 0 && $(this).parent().find(".js-thumb-val").val() < 10) {
             $(this).parent().find(".js-thumb-val").val(+$(this).parent().find(".js-thumb-val").val() + 1)
         }
     });
 
-    $(".js-thumb-down").click(function() {
-        if ($(this).parent().find(".js-thumb-val").val() > 0 && $(this).parent().find(".js-thumb-val").val() <= 10 ) {
+    $(".js-thumb-down").click(function () {
+        if ($(this).parent().find(".js-thumb-val").val() > 0 && $(this).parent().find(".js-thumb-val").val() <= 10) {
             $(this).parent().find(".js-thumb-val").val(+$(this).parent().find(".js-thumb-val").val() - 1)
         }
     });
+
+    /*    var Model ={};
+
+     var View = {
+     thumbUp: $('.js-thumb-up'),
+     thumbDown: $('.js-thumb-down'),
+     item: $('.js-thumb-val')
+     };
+
+     View.thumbUpClick = function (button) {
+     if ( $(button).parent().find(View.item).val() > 0 && $(button).parent().find(View.item).val() < 10  ) {
+
+     $(button).parent().find(View.item).val(+$(button).parent().find(View.item).val() + 1);
+
+     }
+     };
+
+     var Controller = {};
+
+     Controller.bindThumbEvents = function() {
+     View.thumbUp.click(function() {
+     View.thumbUpClick(this);
+     })
+     };
+
+     Controller.loadView = function() {
+     Controller.bindThumbEvents();
+     };
+
+     Controller.loadView();*/
+
 }
 
 $(document).ready(function () {
     bindClickEvents();
-
-
 });
 
 
